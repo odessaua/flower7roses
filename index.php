8e069c559c7380bb716b497a39b81bc8c5080305
Andrey Korol <andkorol@gmail.com>
2017-06-14 14:47:51 +0300

    Local domain was changed

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $yii=dirname(__FILE__).'/framework/yii.php';
 //$config_file = (($_SERVER['REMOTE_ADDR'] == '127.0.0.1') || ($_SERVER['REMOTE_ADDR'] == '185.68.16.179')) ? 'local.php' : 'main.php';
 
 //$config_file = (($_SERVER['HTTP_HOST'] == 'flowers3.loc') || ($_SERVER['HTTP_HOST'] == 'id20.andkorol.com')) ? 'local.php' : 'main.php';
-$config_file = (($_SERVER['HTTP_HOST'] == 'flowers3.loc')) ? 'local.php' : 'main.php';
+$config_file = (($_SERVER['HTTP_HOST'] == '7roses.loc')) ? 'local.php' : 'main.php';
 $config=dirname(__FILE__).'/protected/config/' . $config_file;
 
 defined('YII_DEBUG') or define('YII_DEBUG', true);
