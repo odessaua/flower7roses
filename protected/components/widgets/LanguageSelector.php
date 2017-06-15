31b0574f85d18270b57d8546cfe6a7860879994a
Andrey Korol <andkorol@gmail.com>
2017-06-15 19:35:24 +0300

    Stage 4 Task 1 Part 3: Language Selector hotfix.

--- protected/components/widgets/LanguageSelector.php
+++ protected/components/widgets/LanguageSelector.php
@@ -4,8 +4,8 @@ class LanguageSelector extends CWidget
     public function run()
     {
         $currentLang = Yii::app()->language;
-        $languages = Yii::app()->params->languages;
-        $defaultLanguage = Yii::app()->params->defaultLanguage;
+        $languages = Yii::app()->params['languages'];
+        $defaultLanguage = Yii::app()->params['defaultLanguage'];
         $this->render('languageSelector', array(
             'currentLang' => $currentLang,
             'languages'=>$languages,
