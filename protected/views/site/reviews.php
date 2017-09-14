519bd74ef7f6017c5e32476e01ec655886a1062b
odessaua <sadovaya@te.net.ua>
2017-09-14 20:45:50 +0300

    https

--- protected/views/site/reviews.php
+++ protected/views/site/reviews.php
@@ -1,6 +1,6 @@
 <?php
 //var_dump($reviews);
-$currentUrl = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$currentUrl = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 ?>
 <style>
     span.required{
