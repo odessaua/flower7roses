46916b9443619f6a7e533292fe60724fb00c40fd
odessaua <sadovaya@te.net.ua>
2017-09-19 12:47:34 +0300

    portmone

--- themes/default/views/orders/cart/view.php
+++ themes/default/views/orders/cart/view.php
@@ -108,9 +108,31 @@ input[type=button]:active, input[type=submit]:active, input[type=reset]:active, 
 			<tr><td valign=top>
 <ul class="payment-list">
 
- <?php if(!empty($payments['WayForPay'])): ?>
+ 
+
+<?php if(!empty($payments['Portmone'])): ?>
                    <li class="selected">
-                        <input type="radio" name="payment" id="payment4" value="<?= (!empty($payments['WayForPay']->id)) ? $payments['WayForPay']->id : 0; ?>" checked /><label for="payment4">
+                        <input type="radio" name="payment" id="payment1" value="<?= (!empty($payments['Portmone']->id)) ? $payments['Portmone']->id : 0; ?>" checked  /><label for="payment1">					   
+<div class="button"><img src="\uploads\visa_portmone75-150.png"></div></label>
+                        <div class="help-tip">
+                            <?php if(!empty($payments['Portmone'])): ?>
+                            <p>
+                                <strong>Portmone</strong>: <?= strip_tags($payments['Portmone']->description); ?>
+                                <br><a href="https://www.portmone.com.ua/r3/<?= $this->language_info['code']; ?>/" target=_blank>https://www.portmone.com.ua</a></p>
+                            <?php else: ?>
+                            <p>
+                                <strong>Portmone</strong>: online credit card processing. All credit card transactions are encrypted. Accept Visa and MasterCard.
+                                <br><a href="https://www.portmone.com.ua/r3/<?= $this->language_info['code']; ?>/" target=_blank>https://www.portmone.com.ua</a></p>
+                            <?php endif; ?>
+                        </div>
+						<span class="price sum"><?echo $symbol.StoreProduct::formatPrice($model->full_price*$rate)."</span> " ;?>
+					</li>
+<hr width=100%>
+<?php endif; ?>
+
+<?php if(!empty($payments['WayForPay'])): ?>
+                   <li>
+                        <input type="radio" name="payment" id="payment4" value="<?= (!empty($payments['WayForPay']->id)) ? $payments['WayForPay']->id : 0; ?>"/><label for="payment4">
 <div class="button"><img src="\uploads\visa_wayforpay75-150.png"></div></label>
                        <div class="help-tip">
                        <?php if(!empty($payments['WayForPay'])): ?>
@@ -126,26 +148,6 @@ input[type=button]:active, input[type=submit]:active, input[type=reset]:active, 
 					   <span class="price sum"><?echo $symbol.StoreProduct::formatPrice($model->full_price*$rate)."</span> " ;?>
 					</li>
 					
-<hr width=100%>
-<?php endif; ?>
-
-<?php if(!empty($payments['Portmone'])): ?>
-                   <li>
-                        <input type="radio" name="payment" id="payment1" value="<?= (!empty($payments['Portmone']->id)) ? $payments['Portmone']->id : 0; ?>" /><label for="payment1">					   
-<div class="button"><img src="\uploads\visa_portmone75-150.png"></div></label>
-                        <div class="help-tip">
-                            <?php if(!empty($payments['Portmone'])): ?>
-                            <p>
-                                <strong>Portmone</strong>: <?= strip_tags($payments['Portmone']->description); ?>
-                                <br><a href="https://www.portmone.com.ua/r3/<?= $this->language_info['code']; ?>/" target=_blank>https://www.portmone.com.ua</a></p>
-                            <?php else: ?>
-                            <p>
-                                <strong>Portmone</strong>: online credit card processing. All credit card transactions are encrypted. Accept Visa and MasterCard.
-                                <br><a href="https://www.portmone.com.ua/r3/<?= $this->language_info['code']; ?>/" target=_blank>https://www.portmone.com.ua</a></p>
-                            <?php endif; ?>
-                        </div>
-						<span class="price sum"><?echo $symbol.StoreProduct::formatPrice($model->full_price*$rate)."</span> " ;?>
-					</li>
 <hr width=100%>
 <?php endif; ?>
 
