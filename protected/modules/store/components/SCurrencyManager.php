a547ee467a949c2ac8f9d09cac8f37348f84056b
Andrey Korol <andkorol@gmail.com>
2017-10-28 01:30:43 +0300

    #10: set default currency by IP

--- protected/modules/store/components/SCurrencyManager.php
+++ protected/modules/store/components/SCurrencyManager.php
@@ -44,7 +44,7 @@ class SCurrencyManager extends CComponent
 				$this->_default = $currency;
 		}
 
-		$this->setActive($this->detectActive()->id);
+		$this->setCurrencyByIP();
 	}
 
 	/**
@@ -140,4 +140,75 @@ class SCurrencyManager extends CComponent
 
 		return $currencies;
 	}
+
+    /**
+     * geo-информация о пользователе
+     * @return array|mixed
+     */
+    public function geoIpInfo()
+    {
+        $ipAddress = Yii::app()->request->userHostAddress; // real user IP
+//        $ipAddress = '192.196.142.22'; // test France
+//        $ipAddress = '188.163.97.7'; // test Ukraine
+//        $ipAddress = '72.229.28.185'; // test United States
+        $ip_key = "9a531e5be48d22f2df5d421eafbb87c2b376206e7314174e7e7c131104e44dae";
+        $query = "http://api.ipinfodb.com/v3/ip-city/?key=" . $ip_key . "&ip=" . $ipAddress . "&format=json";
+        $json = file_get_contents($query);
+        $data = json_decode($json, true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            $data = array();
+        }
+        return $data;
+    }
+
+    /**
+     * валюта по IP-информации о пользователе:
+     * Украина = гривна
+     * Европа = евро
+     * Остальные = доллар
+     */
+    public function setCurrencyByIP()
+    {
+//        Yii::app()->session['currency'] = ''; // uncomment for testing
+        // проверяем валюту в сессии
+        $sessCurrency = Yii::app()->session['currency'];
+        if(!empty($sessCurrency)){
+            // пользователь сам выбрал валюту
+            // или её уже назначили по IP ранее в этой сессии пользователя
+            // делаем активной валюту из сессии
+            $this->setActive($sessCurrency);
+            return true;
+        }
+
+        $ip_info = $this->geoIpInfo();
+        $currencies = $this->getCurrencies();
+        if(!empty($ip_info['countryCode']) && !empty($currencies)){
+            $euro_countries = array(
+                'AT', 'BE', 'CY', 'EE', 'FI', 'FR', 'DE', 'GR', 'IE', 'IT',
+                'LV', 'LT', 'LU', 'MT', 'NL', 'PT', 'SK', 'SI', 'ES',
+            );
+            $ua = 'UA';
+            $set_iso = '';
+            if(in_array($ip_info['countryCode'], $euro_countries)){
+                // EUR
+                $set_iso = 'EUR';
+            }
+            elseif($ip_info['countryCode'] == $ua){
+                // UAH
+                $set_iso = 'UAH';
+            }
+            if(!empty($set_iso)){
+                // устанавливаем новую активную валюту по IP
+                foreach ($currencies as $currency) {
+                    if($currency->iso == $set_iso){
+                        $this->setActive($currency->id);
+                        break;
+                    }
+                }
+                return true;
+            }
+        }
+        $this->setActive($this->detectActive()->id); // default
+        return true;
+    }
 }
