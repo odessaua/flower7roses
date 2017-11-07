a093f50e2650645d1d213737554a59187ffb0685
odessaua <sadovaya@te.net.ua>
2017-11-07 18:47:24 +0200

    Delivery photos

--- themes/default/views/store/frontProduct/view.php
+++ themes/default/views/store/frontProduct/view.php
@@ -266,10 +266,9 @@ $img_title = (!empty($model->img_title)) ? $model->img_title : $model->name;
 		                <!-- <div class="title">г. Одесса</div> -->
 		            </div>
 		            <?php } ?>
-					<div style="text-align:right"><a href="/product/photos/id/<?=$model->id?>" title=""><?=Yii::t('StoreModule.core','View all delivery photos')?></a></div>
-	             <?php } ?>
 	        </div>
-			
+			<div style="text-align:right"><a href="/product/photos/id/<?=$model->id?>" title=""><?=Yii::t('StoreModule.core','View all delivery photos')?></a></div>
+		 <?php } ?>
 	    </div>
 	    <!-- b-last-photos (end) -->
 <!-- 	</div> -->
